e53f35313339200f2f9273e9742362598fd4498d
Elkin Rodriguez <eferneyrod@gmail.com>
2020-09-08 18:20:45 -0500

    Metodo imprimir

---
+++ Problema3.py
@@ -0,0 +1,6 @@
+def imprimir(cadena):
+    caracter1 = cadena[0]
+    caracter2 = cadena[-1]
+    print("Primer caracter de "+ cadena +": "+caracter1+" y ultimo caracter de "+cadena+": "+caracter2)
+
+imprimir("Elkin")
